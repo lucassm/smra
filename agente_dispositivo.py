e9ad084a8195a4942c6eedeee318c1e958d1f244
Lucas Melo <lucas.silveiramelo@gmail.com>
2015-04-29 12:13:27 -0300

    Inserção do comportamento de identificação junto ao agente alimentador e do comportamento de envio de comandos de abertura e fechamento aos IED via protocolo MMS IEC61850

--- agente_dispositivo.py
+++ agente_dispositivo.py
@@ -10,6 +10,7 @@ from pade.behaviours.protocols import FipaRequestProtocol
 import json
 import numpy as np
 
+import iec61850
 #
 # Comportamentos:
 #
@@ -19,7 +20,7 @@ import numpy as np
 #                   o setor sob falta e envia comando de abertura para os
 #                   dispositivos de fronteira do setor faltoso;
 #
-#   CompRequest_3 : Comportamento do tipo FIPA-Request Iniciante que
+#   CompRequest_2 : Comportamento do tipo FIPA-Request Iniciante que
 #                   o agente AD executa ao enviar a mensagem que
 #                   informa ao agente AA sobre a ocorrencia de uma
 #                   falta no sistema;
@@ -31,6 +32,7 @@ class CompRequest1(FipaRequestProtocol):
     def __init__(self, agent):
         super(CompRequest1, self).__init__(
             agent=agent, message=None, is_initiator=False)
+        self.alimentador = self.agent.agente_alimentador.alimentador
 
     def handle_request(self, message):
 
@@ -45,11 +47,18 @@ class CompRequest1(FipaRequestProtocol):
             resposta.set_content(json.dumps({'ref': 'R_01'}, indent=4))
             self.agent.send(resposta)
 
-            # chaves_de_isolacao = self.encontrar_chaves_de_isolacao()
+            chaves_de_isolacao = self.encontrar_chaves_de_isolacao()
 
             # # # # # #
-            # TODO: envia mensagem MMS para abrir as chaves de isolacao!
+            # Envia mensagem MMS para abrir as chaves de isolacao!
             # # # # # #
+            comunicacao = self.agent.agente_alimentador.comunicacao
+
+            for chave in chaves_de_isolacao:
+                iec61850.createConnection(comunicacao[chave].ip,
+                                          int(comunicacao[chave].porta))
+                iec61850.operate('SEL_751_1PRO/BKR1CSWI1.Pos', False)
+                iec61850.closeConnection()
 
             message = ACLMessage(ACLMessage.REQUEST)
             message.set_protocol(ACLMessage.FIPA_REQUEST_PROTOCOL)
@@ -63,7 +72,7 @@ class CompRequest1(FipaRequestProtocol):
             message.add_receiver(self.agent.agente_alimentador.aid.localname)
 
             # lança comportamento
-            comp = CompRequest4(self.agent, message)
+            comp = CompRequest2(self.agent, message)
             self.agent.behaviours.append(comp)
             comp.on_start()
 
@@ -97,10 +106,10 @@ class CompRequest1(FipaRequestProtocol):
         return chaves_de_isolacao
 
 
-class CompRequest4(FipaRequestProtocol):
+class CompRequest2(FipaRequestProtocol):
 
     def __init__(self, agent, message):
-        super(CompRequest4, self).__init__(agent=agent,
+        super(CompRequest2, self).__init__(agent=agent,
                                            message=message,
                                            is_initiator=True)
 
@@ -121,13 +130,82 @@ class CompRequest4(FipaRequestProtocol):
             display_message(self.agent.aid.name, 'Mensagem INFORM recebida')
 
 
+class CompRequest3(FipaRequestProtocol):
+    def __init__(self, agent):
+        super(CompRequest3, self).__init__(agent=agent,
+                                           message=None,
+                                           is_initiator=False)
+
+    def handle_request(self, message):
+        # carrega conteudo da mensagem recebida
+        self.content = json.loads(message.content)
+
+        if self.content['ref'] == 'R_05':
+            display_message(self.agent.aid.name, 'Mensagem REQUEST recebida')
+
+            chaves = self.content['dados']['chaves']
+            estados = self.content['dados']['estados']
+            comunicacao = self.agent.agente_alimentador.comunicacao
+
+            for i, j in zip(chaves, estados):
+                iec61850.createConnection(comunicacao[i].ip,
+                                          int(comunicacao[i].porta))
+                if j == 1:
+                    iec61850.operate('SEL_751_1PRO/BKR1CSWI1.Pos', True)
+                else:
+                    iec61850.operate('SEL_751_1PRO/BKR1CSWI1.Pos', False)
+                iec61850.closeConnection()
+
+
+class CompRequest4(FipaRequestProtocol):
+
+    def __init__(self, agent, message):
+        super(CompRequest4, self).__init__(agent=agent,
+                                           message=message,
+                                           is_initiator=True)
+
+    def handle_agree(self, message):
+
+        # carrega conteudo da mensagem recebida
+        self.content = json.loads(message.content)
+
+        if self.content['ref'] == 'R_06':
+            display_message(self.agent.aid.name, 'Mensagem AGREE recebida')
+
+    def handle_inform(self, message):
+
+        # carrega conteudo da mensagem recebida
+        self.content = json.loads(message.content)
+
+        if self.content['ref'] == 'R_06':
+            display_message(self.agent.aid.name, 'Mensagem INFORM recebida')
+
+
 class AgenteDispositivo(Agent):
 
     def __init__(self, aid, aa):
         super(AgenteDispositivo, self).__init__(aid=aid, debug=False)
         self.agente_alimentador = aa
+
+        # Comportamento de Inicialização
+        message = ACLMessage(ACLMessage.REQUEST)
+        message.set_protocol(ACLMessage.FIPA_REQUEST_PROTOCOL)
+        message.set_content(json.dumps({'ref': 'R_06',
+                                        'dados': 'AUTH'}, indent=4)
+                            )
+        message.add_receiver(self.agente_alimentador.aid.localname)
+
+        comp_request_4 = CompRequest4(self, message)
+
+        # Comportamento que aguarda por ocorrências na rede
         comp_request_1 = CompRequest1(self)
+
+        # Comportamento de comando de IEDs
+        comp_request_3 = CompRequest3(self)
+
+        self.behaviours.append(comp_request_4)
         self.behaviours.append(comp_request_1)
+        self.behaviours.append(comp_request_3)
 
 if __name__ == '__main__':
     ad_1 = AgenteDispositivo(AID(name='AD_1'))
