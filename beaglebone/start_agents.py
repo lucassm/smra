caf9d55cf43777ecd4c65460a6a7027c42c4cae7
Lucas Melo <lucas.silveiramelo@gmail.com>
2015-06-03 20:57:51 -0300

    alteracoes nos arquivos start_agents.py

--- beaglebone/start_agents.py
+++ beaglebone/start_agents.py
@@ -1,12 +1,17 @@
 from pade.misc.common import start_loop
 from agente_alimentador import AgenteAlimentador
+from agente_dispositivo import AgenteDispositivo
 from pade.acl.aid import AID
 
 if __name__ == '__main__':
 
     # Agentes Alimentadores
-    aa_1 = AgenteAlimentador(AID('S2_AL1'))
-    aa_1.ams({'name': '192.168.0.100', 'port': 8000})
+    aa_1 = AgenteAlimentador(AID('S2_AL1@192.168.24.102:4001'))
+    aa_1.ams({'name': '192.168.24.101', 'port': 8000})
 
-    agentes = [aa_1]
+    # Agentes Dispositivos
+    ad_1 = AgenteDispositivo(AID('S2_AD1@192.168.24.102:4002'))
+    ad_1.ams({'name': '192.168.24.101', 'port': 8000})
+
+    agentes = [aa_1, ad_1]
     start_loop(agentes, gui=False)
--- raspberry/start_agents.py
+++ raspberry/start_agents.py
@@ -1,12 +1,17 @@
 from pade.misc.common import start_loop
 from agente_alimentador import AgenteAlimentador
+from agente_dispositivo import AgenteDispositivo
 from pade.acl.aid import AID
 
 if __name__ == '__main__':
 
     # Agentes Alimentadores
-    aa_1 = AgenteAlimentador(AID('S1_AL2'))
-    aa_1.ams({'name': '192.168.0.100', 'port': 8000})
+    aa_1 = AgenteAlimentador(AID('S1_AL2@192.168.24.100:4001'))
+    aa_1.ams({'name': '192.168.24.101', 'port': 8000})
 
-    agentes = [aa_1]
+    # Agentes Dispositivos
+    ad_1 = AgenteDispositivo(AID('S1_AD2@192.168.24.100:4002'))
+    ad_1.ams({'name': '192.168.24.101', 'port': 8000})
+
+    agentes = [aa_1, ad_1]
     start_loop(agentes, gui=False)
