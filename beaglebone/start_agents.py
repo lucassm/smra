362c7f949a1dbb8faf7805a37543e4b0c04289d0
lucassm <lucas.silveiramelo@gmail.com>
2015-06-04 23:40:25 -0300

    correcoes nos comandos de inicializacao dos agentes nas pastas raspberry e beaglebone

--- beaglebone/start_agents.py
+++ beaglebone/start_agents.py
@@ -7,11 +7,11 @@ if __name__ == '__main__':
 
     # Agentes Alimentadores
     aa_1 = AgenteAlimentador(AID('S2_AL1@192.168.24.102:4001'))
-    aa_1.ams({'name': '192.168.24.101', 'port': 8000})
+    aa_1.ams = {'name': '192.168.24.101', 'port': 8000}
 
     # Agentes Dispositivos
-    ad_1 = AgenteDispositivo(AID('S2_AD1@192.168.24.102:4002'))
-    ad_1.ams({'name': '192.168.24.101', 'port': 8000})
+    ad_1 = AgenteDispositivo(AID('S2_AD1@192.168.24.102:4002'), aa_1)
+    ad_1.ams = {'name': '192.168.24.101', 'port': 8000}
 
     agentes = [aa_1, ad_1]
     start_loop(agentes, gui=False)
--- raspberry/start_agents.py
+++ raspberry/start_agents.py
@@ -7,11 +7,11 @@ if __name__ == '__main__':
 
     # Agentes Alimentadores
     aa_1 = AgenteAlimentador(AID('S1_AL2@192.168.24.100:4001'))
-    aa_1.ams({'name': '192.168.24.101', 'port': 8000})
+    aa_1.ams = {'name': '192.168.24.101', 'port': 8000}
 
     # Agentes Dispositivos
-    ad_1 = AgenteDispositivo(AID('S1_AD2@192.168.24.100:4002'))
-    ad_1.ams({'name': '192.168.24.101', 'port': 8000})
+    ad_1 = AgenteDispositivo(AID('S1_AD2@192.168.24.100:4002'), aa_1)
+    ad_1.ams = {'name': '192.168.24.101', 'port': 8000}
 
     agentes = [aa_1, ad_1]
     start_loop(agentes, gui=False)
